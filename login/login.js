71c05951afb472278b129ddd1c0217e99eb45306
Plusquam95 <erwinramos17@gmail.com>
2023-09-24 21:24:25 -0600

    Login

---
+++ login/login.js
@@ -0,0 +1,16 @@
+const correo = document.getElementById("correo");
+const pw = document.getElementById("pw-login");
+const login = document.getElementById("Inicio-sesion");
+
+console.log(login);
+
+login.addEventListener("click", loginCnl);
+function loginCnl() {
+  if (pw.value === "cnl" && correo.value === "cnl") {
+    login.href = "carreras/carreras.html";
+  } else {
+    alert("Usuario o contraseña incorrecta");
+    correo.value = "";
+    pw.value = "";
+  }
+}
