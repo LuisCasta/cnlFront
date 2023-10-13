bcac21a0f9da2d93610ac2a700b5f915549ea74a
Plusquam95 <erwinramos17@gmail.com>
2023-10-13 07:25:45 -0600

    Tab

--- mentor/about-unit/about.js
+++ mentor/about-unit/about.js
@@ -14,19 +14,26 @@ async function loadAllLessonsByUnit() {
   const lessons = await getAllByUnit(idUnit);
 
   if (lessons.code != 200) {
-    console.log(`Error ${newLesson.message}`);
+    // console.log(`Error ${newLesson.message}`);
   } else {
     const countlessons = document.getElementById("spanTitle");
     countlessons.textContent = lessons.data.length;
     lessons.data.forEach((lesson) => {
-      const { name, description, id, dateStart } = lesson;
+      const { name, description, id } = lesson;
       hrefUnidad.href = `../unit/unit.html?idCurso=${idCourse}&idUnit=${id}`;
       lessonHtml += `
         <tr>
             <td data-cell="Id">${id}</td>
             <td data-cell="Nombre">${name}</td>
             <td data-cell="Descripción">${description}</td>
-            <td data-cell="Inicio">${dateStart}</td>
+            <td data-cell="Actions">
+            <div class="actions">
+              <button data-id="${id}" class="eliminar"><i class='bx bx-trash'></i></button>
+              <button data-id="${id}" class="editar"><i class='bx bx-edit' ></i></button>
+              </div>
+          </td>
+          <td data-cell="Lesson+">
+          <a href="../lessons/lessons.html?idCurso=${idCourse}&idUnit=${id}"><i class='bx bx-bookmark-plus'></i></a></td>
         </tr>
         `;
     });
@@ -54,9 +61,35 @@ async function loadActivityByUnit() {
             <td data-cell="Nombre">${name}</td>
             <td data-cell="Descripción">${description}</td>
             <td data-cell="Inicio">${dateStart}</td>
+            <td data-cell="Actividad+">
+            <button><i class='bx bxs-vial'></i></button>
+            </td>
         </tr>
         `;
     });
     tableAct.innerHTML = activityHtml;
+  }
+}
+
+//Listar los alumnos por Curso
+let studentIdCourse;
+async function loadStudentByIdCourse() {
+  const studentsidCourse = await CourseStudGetByCourse(idCourse);
+  if (studentsidCourse.code != 200) {
+    console.log(newStudentsIdCourse.message);
+  } else {
+    studentsidCourse.data.forEach((studentId) => {
+      const { id, name, firstName } = studentsidCourse;
+      studentIdCourse += `
+      <tr>
+      <td data-cell="Id">${id}</td>
+      <td data-cell="Nombre">${name}</td>
+      <td data-cell="Descripción">${firstName}</td>
+      <td data-cell="Actions">
+      <button><i class='bx bxs-user-check'></i></button>
+      </td>
+  </tr>
+  `;
+    });
   }
 }
--- mentor/about-unit/tabs.js
+++ mentor/about-unit/tabs.js
@@ -1,4 +1,71 @@
 const btnLesson = document.getElementById("clases");
 const btnActiv = document.getElementById("activ");
-const students = document.getElementById("students");
-const vCall = document.getElementById("videollamdas");
+const btnStud = document.getElementById("students");
+const btnCall = document.getElementById("calls");
+// contenedores
+const contLesson = document.getElementById("div-clases");
+const contActiv = document.getElementById("div-actividades");
+const conStud = document.getElementById("div-alumnos");
+const contCall = document.getElementById("div-call");
+
+// contLesson.classList.add("disabled");
+conStud.classList.add("disabled");
+contCall.classList.add("disabled");
+contActiv.classList.add("disabled");
+
+btnLesson.classList.add("button-bottom");
+
+btnActiv.addEventListener("click", ocultarDivs);
+btnLesson.addEventListener("click", ocultarDivs1);
+btnStud.addEventListener("click", ocultarDivs2);
+btnCall.addEventListener("click", ocultarDivs3);
+
+function ocultarDivs() {
+  // CLASES EN CONTAINER
+  contLesson.classList.add("disabled");
+  conStud.classList.add("disabled");
+  contCall.classList.add("disabled");
+  contActiv.classList.remove("disabled");
+
+  //   clases en Botones
+  btnActiv.classList.add("button-bottom");
+  btnLesson.classList.remove("button-bottom");
+  btnStud.classList.remove("button-bottom");
+  btnCall.classList.remove("button-bottom");
+}
+
+function ocultarDivs1() {
+  contLesson.classList.remove("disabled");
+  conStud.classList.add("disabled");
+  contCall.classList.add("disabled");
+  contActiv.classList.add("disabled");
+  //   clases en Botones
+  btnLesson.classList.add("button-bottom");
+  btnActiv.classList.remove("button-bottom");
+  btnStud.classList.remove("button-bottom");
+  btnCall.classList.remove("button-bottom");
+}
+
+function ocultarDivs2() {
+  contLesson.classList.add("disabled");
+  conStud.classList.remove("disabled");
+  contCall.classList.add("disabled");
+  contActiv.classList.add("disabled");
+  //   clases en Botones
+  btnStud.classList.add("button-bottom");
+  btnCall.classList.remove("button-bottom");
+  btnLesson.classList.remove("button-bottom");
+  btnActiv.classList.remove("button-bottom");
+}
+
+function ocultarDivs3() {
+  contLesson.classList.add("disabled");
+  conStud.classList.add("disabled");
+  contCall.classList.remove("disabled");
+  contActiv.classList.add("disabled");
+  //   clases en Botones
+  btnCall.classList.add("button-bottom");
+  btnStud.classList.remove("button-bottom");
+  btnLesson.classList.remove("button-bottom");
+  btnActiv.classList.remove("button-bottom");
+}
