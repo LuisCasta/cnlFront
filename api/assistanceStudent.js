3011b37cf35f8d885a0efef6f0a22b3cd8679c8f
Tanya <anyrcgm@gmail.com>
2023-10-12 18:12:27 -0600

    tomar asistencia .. create

---
+++ api/assistanceStudent.js
@@ -0,0 +1,24 @@
+/** Funcion para dar de alta un alumno en un curso
+*
+*/
+const create = async (data) => {
+    const { idAssistance, idStudent } = data;
+  
+    if ( idStudent == "" || idStudent == null || idStudent == undefined || idLesson == " " )
+      return { code: 400, message: `Error, el campo idStudent es inválido` };
+  
+    if ( idAssistance == "" || idAssistance == null || idAssistance == undefined || idCourse == " " )
+      return { code: 400, message: `Error, el campo idAssistance es inválido` };
+  
+    const assistanceStudent = await postDataC("assistanceStudent/", 
+        { idCourse, idStudent });
+  
+    if (assistanceStudent.status != 200)
+      return {
+        code: 400,
+        message: `Error al tomar asistencia`,
+        error: assistanceStudent.data.message,
+      };
+  
+    return { code: 200, data: assistanceStudent.data.data };
+  };
