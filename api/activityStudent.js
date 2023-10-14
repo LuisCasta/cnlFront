2d9daf75d921f128388d50b9af8c58773627ed82
Tanya <anyrcgm@gmail.com>
2023-10-14 14:23:01 -0600

    cambios a getByCourseActivityStudent

--- api/activityStudent.js
+++ api/activityStudent.js
@@ -2,9 +2,18 @@
  * Obtiene todas las actividades de un idUnit
  *
  * */
-const getByCourseActivityStudent = async (idCurso) => {
-  const activities = await getApi(`activityStudent/all/${idCourse}`);
+const getByCourseActivityStudent = async (idCurso, tipo) => {
 
+  if (idCurso == "" || idCurso == null || idCurso == undefined || idCurso == " ")
+    return { code: 400, message: `Error, el campo idCurso es inválido` };
+
+  var activities;
+  if(tipo == "agenda") {
+    activities = await getApi(`activityStudent/all/${idCurso}`);
+  } else {
+    activities = await getApi(`activityStudent/all2/${idCurso}`);
+  }
+  
   if (activities.status != 200)
     return {
       code: 400,
