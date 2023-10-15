5b8770f114d36c98db22702a74999580ccc9c12e
LuisCasta <luis.alankasta@hotmail.com>
2023-10-14 21:25:36 -0600

    Fix activity daily

--- api/activityStudent.js
+++ api/activityStudent.js
@@ -2,17 +2,32 @@
  * Obtiene todas las actividades de un idUnit
  *
  * */
-const getByCourseActivityStudent = async (idCurso, tipo) => {
+const getByCourseActivityStudent = async (idCurso, idStudent) => {
 
   if (idCurso == "" || idCurso == null || idCurso == undefined || idCurso == " ")
     return { code: 400, message: `Error, el campo idCurso es inválido` };
 
-  var activities;
-  if(tipo == "agenda") {
-    activities = await getApi(`activityStudent/all/${idCurso}`);
-  } else {
-    activities = await getApi(`activityStudent/all2/${idCurso}`);
-  }
+  
+    const activities = await getApi(`activityStudent/all2/${idCurso}/${idStudent}`);
+  
+  if (activities.status != 200)
+    return {
+      code: 400,
+      message: `Error al obtener las actividades`,
+      error: activities.data.message,
+    };
+
+  return { code: 200, data: activities.data.data };
+};
+
+const getActivitiesDaily = async (idStudent) => {
+
+  if (idCurso == "" || idCurso == null || idCurso == undefined || idCurso == " ")
+    return { code: 400, message: `Error, el campo idCurso es inválido` };
+
+  
+    const activities = await getApi(`activityStudent/all/${idStudent}`);
+  
   
   if (activities.status != 200)
     return {
