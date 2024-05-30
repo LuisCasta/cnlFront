c72c3e1c0b09feef4491c169bfeebeeff9a972e0
plusquam <erwinramos17@gmail.com>
2024-05-30 01:20:10 -0600

    Retícula

--- mentor/mentor/reticula.js
+++ mentor/mentor/reticula.js
@@ -93,42 +93,423 @@ const derecho = {
       campo4: "H.I.",
     },
   ],
+  tercer: [
+    {
+      name: "DERECHOS HUMANOS Y SUS GARANTÍAS",
+      clave: "LDNE301",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "METODOLOGÍA DE LA INVESTIGACIÓN",
+      clave: "LDNE302",
+      obligatorio: 1,
+      campo1:" CE0203",
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO ROMANO",
+      clave: "LDNE303",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO PROCESAL CIVIL",
+      clave: "LDNE304",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "NEGOCIACIÓN Y MANEJO DEL CONFLICTO",
+      clave: "LDNE305",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "PRÁCTICA FORENSE DEL DERECHO CIVIL Y FAMILIAR",
+      clave: "LDNE305",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+  ],
+  cuarto: [
+    {
+      name: "ARGUMENTACIÓN Y LÓGICA JURÍDICA",
+      clave: "LDNE401",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "MECANISMOS ALTERNOS DE SOLUCIÓN DE CONTROVERSIAS",
+      clave: "LDNE402",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "TEORÍA GENERAL DEL PROCESO",
+      clave: "LDNE403",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO PENAL. TEORÍA GENERAL",
+      clave: "LDNE404",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO PENAL. DELITOS Y PENAS",
+      clave: "LDNE405",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "CRIMINOLOGÍA",
+      clave: "LDNE406",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+  ],
+  quinto: [
+    {
+      name: "LITIGACIÓN ORAL",
+      clave: "LDNE501",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO CONSTITUCIONAL",
+      clave: "LDNE502",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO PROCESAL PENAL",
+      clave: "LDNE503",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO LABORAL",
+      clave: "LDNE504",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO SINDICAL",
+      clave: "LDNE405",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "PRÁCTICA FORENSE DEL SISTEMA PENAL ACUSATORIO",
+      clave: "LDNE506",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+  ],
+  sexto: [
+    {
+      name: "DERECHO ADMINISTRATIVOL",
+      clave: "LDNE601",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO ADMINISTRATIVO SANCIONADOR",
+      clave: "LDNE602",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO MERCANTIL",
+      clave: "LDNE603",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO PROCESAL LABORAL",
+      clave: "LDNE604",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "INGLÉS I",
+      clave: "LDNE605",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO PENITENCIARIO",
+      clave: "LDNE606",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+  ],
+  septimo: [
+    {
+      name: "FILOSOFÍA JURÍDICA",
+      clave: "LDNE701",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "MÉTODOS Y TÉCNICAS JURÍDICAS",
+      clave: "LDNE702",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO FISCAL",
+      clave: "LDNE703",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO PROCESAL CONSTITUCIONAL",
+      clave: "LDNE704",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "INGLÉS II",
+      clave: "LDNE705",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO FINANCIERO",
+      clave: "LDNE706",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+  ],
+  octavo: [
+    {
+      name: "DERECHO ELECTORAL",
+      clave: "LDNE801",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "METODOLOGÍA DE LA INVESTIGACIÓN JURÍDICA",
+      clave: "LDNE802",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO INTERNACIONAL PÚBLICO",
+      clave: "LDNE803",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO DE AMPARO",
+      clave: "LDNE804",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "INGLÉS III",
+      clave: "LDNE805",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO NOTARIAL Y REGISTRAL",
+      clave: "LDNE806",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+  ],
+  noveno: [
+    {
+      name: "ÉTICA PROFESIONAL ",
+      clave: "LDNE901",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "SEMINARIO DE TITULACIÓN",
+      clave: "LDNE902",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO INTERNACIONAL PRIVADO",
+      clave: "LDNE903",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "DERECHO AMBIENTAL",
+      clave: "LDNE904",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "INGLÉS IV",
+      clave: "LDNE905",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+    {
+      name: "PROPIEDAD INTELECTUAL",
+      clave: "LDNE906",
+      obligatorio: 1,
+      campo1: 0,
+      campo2: 96,
+      campo3: 6.0,
+      campo4: "H.I.",
+    },
+  ],
 };
 
-derecho.primero.forEach((materia) => {
-  console.log(materia);
-  const containerREticula = document.getElementById("print-materias");
-  let reticulaHtml = "";
-  const { name, clave, obligatorio, campo1, campo2, campo3, campo4 } = materia;
-  reticulaHtml += `
-      <div class="content-reticula">
-      <div class="claves">
-        <div class="clave"><p>${clave}</p></div>
-        <div class="clave"><p>${campo1}</p></div>
-        <div class="clave"><p>${
-          obligatorio == 1 ? "Obligatorio" : "No obligatorio"
-        }</p></div>
-      </div>
-      <div class="nombre-materia">
-        <h5>${name}</h5>
-      </div>
-      <div class="claves">
-        <div class="clave"><p>${campo2}</p></div>
-        <div class="clave"><p>${campo3}</p></div>
-        <div class="clave"><p>${campo4}</p></div>
-      </div>
-    </div>
-  `;
-  containerREticula.innerHTML += reticulaHtml;
-});
+Object.keys(derecho).forEach(semestre => {
+  const contenedor = document.getElementById('contenedor');
+  // Creamos un contenedor para el semestre
+  const semestreDiv = document.createElement('div');
+  //Materias
+  const divMateria = document.createElement('div');
+  divMateria.className = 'materias';
+ // periodos
+  const divPeriodos = document.createElement('div');
+  divPeriodos.className = 'periodo';
 
-derecho.segundo.forEach((materia) => {
-  console.log(materia);
-  const containerREticula = document.getElementById("print-materias2");
-  let reticulaHtml = "";
-  const { name, clave, obligatorio, campo1, campo2, campo3, campo4 } = materia;
-  reticulaHtml += `
-      <div class="content-reticula">
+  semestreDiv.className = 'container-print-reticula';
+
+  divPeriodos.innerHTML = `<h4>${semestre.charAt(0).toUpperCase() + semestre.slice(1)} Cuatrimestre</h>`;
+  contenedor.appendChild(semestreDiv).appendChild(divMateria).appendChild(divPeriodos);
+
+  // Recorremos los cursos de cada semestre
+  derecho[semestre].forEach(curso => {
+    // Creamos un contenedor para el curso
+    const { name, clave, obligatorio, campo1, campo2, campo3, campo4 } = curso;
+    const cursoDiv = document.createElement('div');
+    cursoDiv.className = 'content-reticula';
+    cursoDiv.innerHTML = `
       <div class="claves">
         <div class="clave"><p>${clave}</p></div>
         <div class="clave"><p>${campo1}</p></div>
@@ -144,7 +525,7 @@ derecho.segundo.forEach((materia) => {
         <div class="clave"><p>${campo3}</p></div>
         <div class="clave"><p>${campo4}</p></div>
       </div>
-    </div>
-  `;
-  containerREticula.innerHTML += reticulaHtml;
+    `;
+    semestreDiv.appendChild(cursoDiv);
+  });
 });
